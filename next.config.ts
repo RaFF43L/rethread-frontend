c5ee9d73184d0b530a6ad7ba4aa0b6aa6cc6e3f8
Raffael Pimentel Ribeiro <raffael.pimentel@anbetec.com.br>
2026-03-11 18:34:50 -0300

    refactor: limpar comentários do next.config.ts
    - Removidos comentários redundantes de remotePatterns
    - Código de configuração mais limpo e direto

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,36 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3001',
+        pathname: '/products/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '4566',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.s3.*.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.s3.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.cloudfront.net',
+      },
+    ],
+    unoptimized: process.env.NEXT_PUBLIC_ENABLE_IMAGE_OPTIMIZATION === 'false',
+  },
+  compress: true,
+  reactStrictMode: true,
 };
 
 export default nextConfig;
